7e831274aed3080810f986bcee0dfb00de83751c
jhonbg93 <84696120+jhonbg93@users.noreply.github.com>
2022-01-25 21:38:07 -0500

    add nabvar and add api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [ characters, setCharacters] = useState([]);
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+  const fetchCharacters = (url) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setCharacters(data.results))
+    .catch(error => console.log(error))
+  }
+  useEffect(() =>{
+    fetchCharacters(initialUrl)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>    
+    <div className="container">
+
     </div>
+    
+    </>
+    
   );
 }
 
